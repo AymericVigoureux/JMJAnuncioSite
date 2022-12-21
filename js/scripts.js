69e0986ded65b45c780269d9942e7c71ff1e4e9d
Etienne Ferrandi <etienne.ferrandi@hotmail.fr>
2022-12-21 12:20:40 +0100

    modif veille de Noël

--- js/scripts.js
+++ js/scripts.js
@@ -44,3 +44,12 @@ function getRemainingTime(){
 
     return "remaining time = " + years + " years, " + months + " months, " + days + " days.";
 }
+
+document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+    el.addEventListener('shown.bs.tab', () => {
+      const target = el.getAttribute('data-bs-target')
+      const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+      bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+    })
+  })
+  
